ee0dd5ac83dcaefdf2cc0a63315cc47b918f814b
Yen-yen Teng <yenyen@yen-yens-air.mynetworksettings.com>
2024-03-14 23:47:52 -0400

    path command fixed

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -4,70 +4,80 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include "wish.h"
 
-#define MAX_PATHS 10
-#define MAX_TOKENS 100
-#define MAX_CMD_LEN 256
+FILE *fptr = NULL;
+char *line = NULL;
+char *paths[MAX_PATHS] = {"/bin/", NULL};
+size_t pathsize = 1;
+char *delimiter = " ";
+
+void clean(void)
+{
+    free(line);
+    fclose(fptr);
+}
+// void *parseInput(void *arg)
+// {
+// }
+
 int main(int argc, char *argv[])
 {
-    FILE *fptr = NULL;
     if (argc > 1)
     {
         fptr = fopen(argv[1], "r");
         if (fptr == NULL)
         {
-            perror("Error opening file");
+            // perror("Error opening file");
+            printError();
         }
     }
-    char *paths[MAX_PATHS];
-    paths[0] = "/bin/";
-    size_t pathsize = 1;
-    char *delimiter = " ";
 
     while (1)
     {
-        char *cmd = NULL;
+        char *line = NULL;
         size_t bufsize = MAX_CMD_LEN;
         size_t characters;
         char *redirection = NULL;
 
         char *token;
 
-        cmd = (char *)malloc(bufsize * sizeof(char));
-        if (cmd == NULL)
+        line = (char *)malloc(bufsize * sizeof(char));
+        if (line == NULL)
         {
             perror("Unable to allocate buffer");
         }
         if (fptr == NULL)
         {
             printf("wish> ");
-            characters = getline(&cmd, &bufsize, stdin);
+            characters = getline(&line, &bufsize, stdin);
         }
         else
         {
-            characters = getline(&cmd, &bufsize, fptr);
+            characters = getline(&line, &bufsize, fptr);
             if (characters == -1)
             {
                 // printf("EOF");
-                fclose(fptr);
-                free(cmd);
+                clean();
                 exit(0);
                 break;
             }
         }
-        if (cmd[0] == '#' || cmd[0] == '\n')
+        if (line[0] == '#' || line[0] == '\n')
         {
             continue;
         }
-        // Replace the newline character with null terminator
-        for (int i = 0; cmd[i] != '\0'; i++)
+        for (int i = 0; line[i] != '\0'; i++)
         {
-            if (cmd[i] == '\t')
-                cmd[i] = ' ';
-            if (cmd[i] == '\n')
-                cmd[i] = '\0';
+            if (line[i] == '\t')
+                line[i] = ' ';
+            if (line[i] == '\n')
+                line[i] = '\0';
+            if (line[i] == '>')
+            {
+            }
         }
-        token = strsep(&cmd, delimiter);
+        token = strsep(&line, delimiter);
         const char *cur_cmd = token;
         char *tokens[MAX_TOKENS];
         size_t token_size = 0;
@@ -76,58 +86,75 @@ int main(int argc, char *argv[])
         {
             if (token[0] == '\0')
             {
-                token = strsep(&cmd, delimiter);
+                token = strsep(&line, delimiter);
                 continue;
             }
             if (strlen(token) == 1 && token[0] == '>')
             {
-                redirection = strsep(&cmd, delimiter);
-                // printf("%s\n", redirection);
+                redirection = strsep(&line, delimiter);
+                if (redirection == NULL)
+                {
+                    printError();
+                    goto end_loop;
+                }
+                printf("%s\n", redirection);
                 break;
             }
             tokens[token_size] = token;
 
             token_size++;
-            token = strsep(&cmd, delimiter);
+            token = strsep(&line, delimiter);
         }
         tokens[token_size] = NULL;
-        for (int i = 0; i < token_size; i++)
-        {
-            printf("%s\n", tokens[i]);
-        }
+        // for (int i = 0; i < token_size; i++)
+        // {
+        //     printf("%s\n", tokens[i]);
+        // }
 
         if (strcmp("exit", cur_cmd) == 0)
         {
-            fclose(fptr);
-            free(cmd);
+            if (token_size != 1)
+            {
+                printError();
+                continue;
+            }
+            clean();
             exit(0);
         }
         else if (strcmp("cd", cur_cmd) == 0)
         {
             if (token_size != 2)
             {
-                perror("cd take exactly one argument");
+                printError();
+                continue;
+                // perror("cd take exactly one argument");
             }
             const char *new_path = tokens[1];
             if (chdir(new_path) != 0)
             {
-                perror("change dir failed");
+                printError();
+                continue;
+                // perror("change dir failed");
             }
         }
         else if (strcmp("path", cur_cmd) == 0)
         {
-            if (token_size == 1)
+            for (size_t i = 0; i < pathsize; i++)
             {
-                perror("specify the path");
-                continue;
+                paths[i] = NULL;
             }
-            size_t path_length = strlen(tokens[1]);
-            if (tokens[1][path_length - 1] != '/')
+            pathsize = 0;
+            for (size_t i = 1; i < token_size; i++)
             {
-                strcat(tokens[1], "/");
+                size_t path_length = strlen(tokens[1]);
+                if (tokens[i][path_length - 1] != '/')
+                {
+                    strcat(tokens[i], "/");
+                }
+                // printf("%s\n", tokens[i]);
+                paths[pathsize] = tokens[i];
+                pathsize++;
             }
-            paths[pathsize] = tokens[1];
-            pathsize++;
         }
         else
         {
@@ -136,7 +163,8 @@ int main(int argc, char *argv[])
             if (ppid < 0)
             {
                 // fork failed
-                fprintf(stderr, "fork failed\n");
+                // fprintf(stderr, "fork failed\n");
+                printError();
                 exit(1);
             }
             else if (ppid == 0)
@@ -147,38 +175,45 @@ int main(int argc, char *argv[])
                     int redirection_fd = open(redirection, O_RDWR | O_CREAT, 0666);
                     if (redirection_fd < 0)
                     {
-                        perror("cannot open the file");
+                        // perror("cannot open the file");
+                        printError();
+                        continue;
                     }
                     if (dup2(redirection_fd, STDOUT_FILENO) < 0)
                     {
-                        perror("cannot write in this file");
+                        // perror("cannot write in this file");
+                        printError();
+                        continue;
                     }
                     dup2(redirection_fd, STDERR_FILENO);
                     if (close(redirection_fd) < 0)
                     {
-                        perror("cannot close the file");
+                        // perror("cannot close the file");
+                        printError();
+                        continue;
                     }
                 }
                 for (size_t i = 0; i < pathsize; i++)
                 {
                     char full_path[256];         // Allocate a buffer for the full path
                     strcpy(full_path, paths[i]); // Copy path1 to full_path
-                    strcat(full_path, cur_cmd);  // Append cmd to full_path
+                    strcat(full_path, cur_cmd);  // Append line to full_path
                     if (access(full_path, X_OK) != -1)
                     {
                         execv(full_path, tokens);
-                        perror("execv");
                     }
                 }
 
                 // If no executable found, print error
-                perror("command not found");
+                printError();
+                exit(0);
             }
             else
             {
                 wait(NULL);
             }
-            free(cmd);
+        end_loop:
+            clean();
         }
     }
     return 0;
